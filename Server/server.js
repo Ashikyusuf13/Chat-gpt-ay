675a0e9aebf4d1726128fcd726b4536acd58b5b2
ashikyusuf13 <ashik13ay@gmail.com>
2025-11-11 17:22:09 +0530

    updated stripe

--- Server/server.js
+++ Server/server.js
@@ -6,21 +6,21 @@ import AuthRouter from "./Routes/AuthRouter.js";
 import chatrouter from "./Routes/Chatrouter.js";
 import messagerouter from "./Routes/MessageRoute.js";
 import creditrouter from "./Routes/Creditsrouts.js";
-/* import { stripewebhooks } from "./Controller/Webhooks.js";
- */
+import { stripewebhooks } from "./Controller/Webhooks.js";
+
 //initial express
 const app = express();
 
 //mongodb server
 await connectDb();
 
-/* //stripe webhook
+//stripe webhook
 app.post(
   "/api/webhook",
   express.raw({ type: "application/json" }),
   stripewebhooks
 );
- */
+
 //middleware
 app.use(cors());
 app.use(express.json());
